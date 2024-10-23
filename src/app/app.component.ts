612fec5d6f40fef0a6fa352a59428edc3ae1a89b
Trevor Higgins <higginst.th@gmail.com>
2024-10-23 09:38:24 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  standalone: true,
+  styleUrl: './app.component.scss'
   
 })
 export class AppComponent {
